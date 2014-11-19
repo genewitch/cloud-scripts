7e3fd300088883631b9fa824d3c3112ab6992ea3
genewitch <genewitch@gmail.com>
2014-11-19 02:05:53 -0600

    Create key2sortedSet.py

---
+++ AWS/key2sortedSet.py
@@ -0,0 +1,26 @@
+import redis
+import string
+
+counter=0
+left=0
+toggle = 1
+"""r = redis.StrictRedis('localhost')"""
+r = redis.StrictRedis(unix_socket_path='/tmp/redis.sock')
+pipe = r.pipeline()
+zkey = "ztest"
+namespaces = "4:*"
+while toggle:
+
+        left, right = r.scan(left, match=namespaces)
+        for word in right:
+                score = r.get(word)
+                pipe.zadd(zkey, float(score), word)
+                counter = counter + 1
+
+        if counter > 10000:
+                pipe.execute()
+                counter = 0
+
+        if left == 0:
+                toggle = 0
+
