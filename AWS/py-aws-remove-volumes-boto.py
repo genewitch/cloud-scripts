a21ce3f590081ac4ca68871134c8c5d0dc82172d
genewitch <genewitch@gmail.com>
2014-11-19 20:24:46 -0600

    Update py-aws-remove-volumes-boto.py

--- AWS/py-aws-remove-volumes-boto.py
+++ AWS/py-aws-remove-volumes-boto.py
@@ -4,9 +4,13 @@ datearg = sys.argv[1]
 #e.g. get_all_volumes() in boto.ec2
 for vol in volumeList:
     if vol.status == "in-use":
+        sleep(10)
+        print("Awake")
         continue
     if datearg in str(vol.create_time):
-        print(vol)
+        print("+++++++++++++++++++\n" + vol)
         print(vol.status + ", create_time:" + vol.create_time)
-        print(vol.delete())
-        print("==========================================================================")
+        if (vol.delete()):
+            print("==============================================")
+        print("failed deletion failed on " + vol + "\n ***********)
+        
