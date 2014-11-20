79f64c82660145898cda62ca667f4f42a7b0af43
genewitch <genewitch@gmail.com>
2014-11-19 20:13:46 -0600

    Create py-aws-remove-volumes-boto.py

---
+++ AWS/py-aws-remove-volumes-boto.py
@@ -0,0 +1,12 @@
+#e.g. 2014-11-19 or 2014 or 2014-10
+datearg = sys.argv[1]
+
+#e.g. get_all_volumes() in boto.ec2
+for vol in volumeList:
+    if vol.status == "in-use":
+        continue
+    if datearg in str(vol.create_time):
+        print(vol)
+        print(vol.status + ", create_time:" + vol.create_time)
+        print(vol.delete())
+        print("==========================================================================")
