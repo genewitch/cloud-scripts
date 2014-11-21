7bb88eb4638a1b920bde3c50550bbdcbc15c881f
genewitch <genewitch@gmail.com>
2014-11-20 18:44:42 -0600

    Update key2sortedSet.py

--- AWS/key2sortedSet.py
+++ AWS/key2sortedSet.py
@@ -5,9 +5,17 @@ counter=0
 left=0
 toggle = 1
 """r = redis.StrictRedis('localhost')"""
+
+#sockets are faster for doing transforms
 r = redis.StrictRedis(unix_socket_path='/tmp/redis.sock')
+#and pipes are faster yet
 pipe = r.pipeline()
+
 zkey = "ztest"
+
+# this program outputs how many NEW members were added to sorted set
+starting = r.zcard(zkey)
+
 while toggle:
 
     left, right = r.scan(left, match="4:*")
@@ -17,8 +25,12 @@ while toggle:
         counter = counter + 1
 
     if counter > 10000:
+        #pipelining 10k responses seems to work ok
         pipe.execute()
         counter = 0
 
     if left == 0:
+        #left starts at 0 and when it is 0 again zscan is complete
         toggle = 0
+
+print(r.zcard(zkey)-starting)
