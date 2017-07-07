c61deedf5fba8ff518b838e37a180d1e086913f8
genewitch <genewitch@gmail.com>
2017-07-07 01:22:34 -0500

    Update rdrand-tmpfs-writer.py
    readme to top

--- rdrand-tmpfs-writer.py
+++ rdrand-tmpfs-writer.py
@@ -1,3 +1,15 @@
+# rdrand-tmpfs-writer.py :
+#
+#  this script, specifically, will write 256000000 characters to stdout. use:
+#
+#         ../rdrand-tmpfs-writer.py > /tmp/foo/rdrand.out 
+#
+#  or so.
+#
+# And if you're using NIST software, it's 1000000 bits per bitstream, 256 bitstreams. Assess that.
+#
+# Further, I'll unpatiently add, only works on python2 if you use easy_install on gentoo.
+
 from rdrand import RdRandom
 import sys
 r = RdRandom()
